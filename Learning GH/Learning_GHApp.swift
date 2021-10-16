2426ed1ad4bcd2bf490a67561cb5b5c5fc83bf65
Flynn Holthouse <holtzy52@icloud.com>
2021-10-16 22:15:48 +0800

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Flynn Holthouse on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
